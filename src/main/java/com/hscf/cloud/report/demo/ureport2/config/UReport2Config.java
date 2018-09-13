97461cc67e17ffc8ebc183a11ce5d4975e8bfc28
Haiwei Liu <carllhw@gmail.com>
2018-09-13 17:19:09 +0800

    remove mybatis-spring-boot-starter and lombok, add spring-boot-starter-jdbc
    Signed-off-by: Haiwei Liu <carllhw@gmail.com>

--- src/main/java/com/hscf/cloud/report/demo/ureport2/config/UReport2Config.java
+++ src/main/java/com/hscf/cloud/report/demo/ureport2/config/UReport2Config.java
@@ -1,13 +1,11 @@
 package com.hscf.cloud.report.demo.ureport2.config;
 
 import com.bstek.ureport.console.UReportServlet;
-import lombok.Data;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-@Data
 @Configuration
 @ImportResource("classpath:ureport-context.xml")
 public class UReport2Config {
